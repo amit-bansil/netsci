3ecb767a99c18156b2acd1701f6ddd5d3a2d65c8
cdelong <cabell.delong@gmail.com>
2014-06-18 13:27:10 -0500

    Initial skill level/uncertainty code

--- robocompviz/process.py
+++ robocompviz/process.py
@@ -1,5 +1,6 @@
 import csv
 
+
 #utility functions
 def readCSV(fileName):
 	with(open(fileName, 'U')) as file_:
@@ -19,6 +20,9 @@ def incrementDict(dict_, key, amount=1):
 		dict_[key] = 0
 
 	dict_[key] += amount
+
+def joinColumns(key, dicts):
+	return ','.join([key] + [str(dict.get(key)) for dict in dicts])
 
 #data structures
 
@@ -68,21 +72,25 @@ winsByRobot = {}
 lossesByRobot = {}
 pointsByRobot = {}
 
+skillLevelByRobot = {}
+uncertaintyByRobot = {}
+
+for ranking in rankings:
+	skillLevelByRobot[ranking['teamnum']] = 3
+	uncertaintyByRobot[ranking['teamnum']] = 1
+
 for competition in competitions:
-	maxRedScoreEver = max(competition['redscore'], maxRedScoreEver)
-	maxBlueScoreEver = max(competition['bluescore'], maxBlueScoreEver)
+	maxRedScoreEver = max(float(competition['redscore']), maxRedScoreEver)
+	maxBlueScoreEver = max(float(competition['bluescore']), maxBlueScoreEver)
 
-	minRedScoreEver = min(competition['redscore'], minRedScoreEver)
-	minBlueScoreEver = min(competition['bluescore'], minBlueScoreEver)
+	minRedScoreEver = min(float(competition['redscore']), minRedScoreEver)
+	minBlueScoreEver = min(float(competition['bluescore']), minBlueScoreEver)
 
-	maxScoreEver = max(competition['redscore'], competition['bluescore'], maxScoreEver)
-	minScoreEver = min(competition['redscore'], competition['bluescore'], minScoreEver)
+	maxScoreEver = max(float(competition['redscore']), float(competition['bluescore']), maxScoreEver)
+	minScoreEver = min(float(competition['redscore']), float(competition['bluescore']), minScoreEver)
 
-	"""
-	redPoints += competition['redscore']
-	bluePoints += competition['bluescore']
-	allPoints = redPoints + bluePoints
-	"""
+	redPoints += float(competition['redscore'])
+	bluePoints += float(competition['bluescore'])
 
 	if competition['winner'] == 'Red':
 		redWins += 1
@@ -97,16 +105,19 @@ for competition in competitions:
 		loser1 = competition['red1']
 		loser2 = competition['red2']
 
+
+
 	incrementDict(winsByRobot, winner1)
 	incrementDict(winsByRobot, winner2)
 	incrementDict(lossesByRobot, loser1)
 	incrementDict(lossesByRobot, loser2)
 
-"""
+
+allPoints = redPoints + bluePoints
+
 redAvg = redPoints/len(competitions)
 blueAvg = bluePoints/len(competitions)
-allAvg = allPoints/len(competitions)
-"""
+allAvg = allPoints/(2*len(competitions))
 
 #print computations
 print 'redWins:', redWins
@@ -122,14 +133,21 @@ print 'blueAvg', blueAvg
 print 'allAvg', allAvg
 
 winsByRobotSorted = winsByRobot.items()
-winsByRobotSorted.sort(key=lambda x:x[1], reverse=True)
+winsByRobotSorted.sort(key=lambda row:row[1], reverse=True)
 print ''
 print 'ROBOT NAME, WINS'
 print '----------'
 print '\n'.join([str(item) for item in winsByRobotSorted])
 
+skillLevelByRobotSorted = skillLevelByRobot.items()
+skillLevelByRobotSorted.sort(key=lambda row:row[1], reverse=True)
+columns = [winsByRobot, lossesByRobot, skillLevelByRobot, uncertaintyByRobot];
+print ''
+print 'ROBOT NAME, WINS, LOSSES, SKILL LEVEL, UNCERTAINTY'
+print '\n'.join([joinColumns(row[0], columns) for row in skillLevelByRobotSorted])
+
 lossesByRobotSorted = lossesByRobot.items()
-lossesByRobotSorted.sort(key=lambda x:x[1], reverse=True)
+lossesByRobotSorted.sort(key=lambda row:row[1], reverse=True)
 print ''
 print 'ROBOT NAME, LOSSES'
 print '----------'
